a00bafd37ad129a86d9f6bfdb40ca264b8dac1ec
correiavictor92@gmail.com <correiavictor92@gmail.com>
2021-01-28 10:47:55 -0300

    codigo ao final da primeira aula da imersao react

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,53 @@
 import styled from 'styled-components'
+import db from '../db.json';
+import Widget from '../src/components/Widget'
+import Footer from '../src/components/Footer'
+import GitHubCorner from '../src/components/GitHubCorner'
+import QuizBackground from '../src/components/QuizBackground'
+import QuizContainer from '../src/components/QuizContainer'
+//import QuizLogo from '/..src/components/QuizLogo'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+//  const BackgroundImage = styled.div`
+//    background-image: url(${db.bg});
+//    flex: 1;
+//    background-size: cover;
+//    background-position: center;
+//  `;
+
+// export const QuizContainer = styled.div`
+//   width: 100%;
+//   max-width: 350px;
+//   padding-top: 45px;
+//   margin: auto 10px;
+//   @media screen and (max-width: 500px){
+//     margin: auto;
+//     padding: 15px;
+//   }
+// `;
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <Widget>
+            <Widget.Header>
+                <h1>Dragon Ball</h1>
+            </Widget.Header>
+          <Widget.Content>
+            <p>lorem ipsum dolor sit amet...</p>
+          </Widget.Content>
+        </Widget>
+
+        <Widget>
+          <Widget.Content>
+            <h1>Quiz da galera</h1>
+
+            <p>lorem ipsum dolor sit amet...</p>
+          </Widget.Content>
+        </Widget>
+        <Footer />
+      </QuizContainer>
+      <GitHubCorner projectUrl="https://github.com/omariosouto"/>
+    </QuizBackground>
+  );
 }
